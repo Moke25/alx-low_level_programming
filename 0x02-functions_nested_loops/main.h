1932388ab4220a1b4daac79d32c937f6cc258884
Michael Omafuaire <michaelomafuaire@gmail.com>
2023-06-21 09:06:58 -0700

    Moke 0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #ifndef main_h
-#define main_hh
+#define main_h
 
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
+int _isslower(int C);
+int _isalpha(int C);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
