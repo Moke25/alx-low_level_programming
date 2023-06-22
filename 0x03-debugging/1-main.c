91edf0c63112152c527aa4824ee88847f607a11a
Michael Omafuaire <michaelomafuaire@gmail.com>
2023-06-22 12:14:40 -0700

    Moke Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
 * main - causes an infinite loop
 * Return: 0
 */
+
 int main(void)
 {
-int i;
+	int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
 /*while (i < 10)*/
@@ -14,4 +16,28 @@ i = 0;
 /*}*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
+}
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinit loop incoming :(\n");
+
+	i=0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
