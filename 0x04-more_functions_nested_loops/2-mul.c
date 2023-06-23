7b4ed1e5e97304177a8af4b7ae3b5005e5fc41d7
Michael Omafuaire <michaelomafuaire@gmail.com>
2023-06-23 10:36:51 -0700

    Moke Project 0x04 functions

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,10 +2,11 @@
 
 /**
  * mul - multiply 2 int
- * @a - first int
- * @b - second int
- * Return: addition of the multiplication
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplicattion
  */
+
 int mul(int a, int b)
 {
 	return (a * b);
